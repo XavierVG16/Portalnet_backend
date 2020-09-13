becb7ea695e1a0a8e5f552955c49dd18b94d2db6
XavierVG <kjviervelasque16@gmail.com>
2020-09-12 19:01:03 -0500

    correcciones seervicios

--- backend/controllers/usuario.controller.js
+++ backend/controllers/usuario.controller.js
@@ -1,43 +1,46 @@
 const pool = require("../database");
 const helpers = require('../lib/helper');
-
 const usuarioCtrl = {};
-usuarioCtrl.getUsuarios = async (req, res) => {
-  const usuarios = await pool.query("select * from usuario");
-  res.json(usuarios);
+usuarioCtrl.getUsuarios = async (req, res, next) => {
+  const equipo = await pool.query("select * from usuarios");
+
+  res.json(equipo);
 };
 
-usuarioCtrl.createUsuario = async (req , res) =>{
-  const {nombre, apellido, ci, telefono, email, pass_usuario, permiso, imageURL, public_id} = req.body;
+usuarioCtrl.createUsuario = async (req, res, next) => {
+  const { nombre, apellido, email, pass_usuario, tipo_usuario } = req.body;
+  console.log(req.body)
+
   const newUsuario = {
-    nombre, apellido, ci, telefono, email, pass_usuario, permiso, imageURL, public_id
+    nombre, apellido, email, pass_usuario, tipo_usuario
   };
   newUsuario.pass_usuario = await helpers.encryptPassword(pass_usuario);
 
-  await pool.query('insert into usuario set ?',newUsuario);
-  res.json({status:'Usuario registrado'});
-}
-usuarioCtrl.getUsuario = async (req, res) =>{
-  const {id} = req.params;
-  const usuario = await pool.query('select * from usuario where id_usuario = ?',[id]);
-  
+  await pool.query('insert into usuario  set ?', newUsuario);
+  res.json({ status: 'usuario creada' });
+};
+usuarioCtrl.getUsuario = async (req, res, next) => {
+  const { id } = req.params;
+  const usuario = await pool.query('select * from usuarios  WHERE idusuario =  ?', [id]);
   res.json(usuario);
-}
-
-usuarioCtrl.editUsuario =  async (req, res) =>{
-  const {id} = req.params;
-  const {nombre, apellido, ci, telefono, email, pass_usuario, permiso} = req.body;
-  const editUsuario = {nombre, apellido, ci, telefono, email, pass_usuario, permiso};
-  editUsuario.pass_usuario = await helpers.encryptPassword(pass_usuario);
-  await pool.query('UPDATE usuario set ? WHERE id_usuario = ?', [editUsuario, id]);
-  res.json({status:'usuario Actualizado'});
 };
 
-usuarioCtrl.deleteUsuario = async (req, res) =>{
-  const {id} = req.params;
-  await pool.query('delete from  usuario where public_id = ?', [id]);
-  res.json({status:'usuario Eliminado'});
+usuarioCtrl.editUsuario = async (req, res, next) => {
+  const { id } = req.params;
 
-}
+  const { nombre, apellido, email, pass_usuario, tipo_usuario } = req.body;
+
+  const editUsuario = {
+    nombre, apellido, email, pass_usuario, tipo_usuario
+  };
+  await pool.query('UPDATE usuarios set ? WHERE idusuario = ?', [editUsuario, id]);
+  res.json({ status: 'usuario Updated' });
+};
+
+usuarioCtrl.deleteUsuario = async (req, res, next) => {
+  const { id } = req.params;
+  await pool.query('DELETE FROM usuarios WHERE idusuario = ?', [id]);
+  res.json({ status: 'usuario Deleted' });
+};
 
 module.exports = usuarioCtrl;
